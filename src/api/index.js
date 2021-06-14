de8124e266032e724508e056eec11fb950379c49
badris abdelilah <32427487+badris101@users.noreply.github.com>
2021-06-14 14:59:33 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import Iptv from "utils/Iptv";
 
 export const getChannels = () => {
   return Iptv.get("/iptv/channels.json").then((res) =>
-    res.data.filter((elm) => elm.category === "Sport")
+    res.data.filter((elm) => elm.category === "Sports")
   );
 };
