cb92130b6a2776090cb24f2591165d239a20df7f
badris abdelilah <32427487+badris101@users.noreply.github.com>
2023-05-29 17:38:25 +0100

    change api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import Iptv from "utils/Iptv";
 
 export const getChannels = () => {
-  return Iptv.get("/iptv/channels.json").then((res) =>
+  return Iptv.get("/api/channels.json").then((res) =>
     res.data.filter((elm) => {
       return elm.categories.length > 0 && elm.categories[0].name === "Sports";
     })
